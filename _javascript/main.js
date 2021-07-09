cfd428b84a7749f591930749e5d9f0a735120193
Drew Hornbein <drew@dhornbein.com>
2021-07-08 22:18:49 -0600

    colorful updates

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,21 @@
+let colors = ['pink','orange','yellow','cyan','blue','purple','red','green']
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+  [...document.getElementsByClassName('js-rainbow')].forEach(el => {
+    let text = el.innerText,
+        colorCount = Math.floor(Math.random() * colors.length);
+    el.innerHTML = '';
+
+    text.split('').forEach(letter => {
+      if (letter != " ") {
+        let sp = document.createElement('span');
+        colorCount = (colorCount + 1 < colors.length) ? colorCount + 1 : 0;
+        sp.setAttribute('class', 'has-text-' + colors[colorCount] );
+        sp.textContent = letter;
+        el.appendChild(sp);
+      } else {
+        el.innerHTML += ' ';
+      }
+    });
+  });
 });
