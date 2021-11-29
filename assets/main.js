a85c92b8d3124b68edd20a5bd7661c7db058c96c
Drew Hornbein <drew@dhornbein.com>
2021-11-29 01:49:04 -0700

    adds mobile optimizations

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
 let colors = ['pink','orange','yellow','cyan','blue','purple','red','green']
 let colorsHex = ['#FF77F9', '#FFA47F', '#F9E800', '#87FFDD', '#35C7FF', '#A992FF', '#FD4796', '#C3FF6F']
 
